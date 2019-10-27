b9120f224fafceeba7d7a653726ddfefffb04ce3
huanghz <827283849@qq.com>
2019-10-27 21:32:22 +0800

    微服务初始化

---
+++ microservice/demo-service03/src/main/java/hhz/demo/Service03Application.java
@@ -0,0 +1,22 @@
+package hhz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName: Service02Application
+ * @Description: TODO(概况)
+ * @author: huanghz
+ * @date: 2019/10/27 下午 04:56
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Service03Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Service03Application.class, args);
+    }
+
+}
