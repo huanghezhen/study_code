c649f38feb30386b0e6f5cc3241d4b45bebc1665
huanghz <827283849@qq.com>
2019-10-31 21:21:49 +0800

    dubbo初始化

--- microservice/demo-service04/src/main/java/hhz/demo/Service04Application.java
+++ microservice/demo-service04/src/main/java/hhz/demo/Service04Application.java
@@ -10,11 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2019/10/27 下午 04:56
  */
 @SpringBootApplication
-public class Service03Application
+public class Service04Application
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(Service03Application.class, args);
+        SpringApplication.run(Service04Application.class, args);
     }
-
 }
