2b45b1eea7bf422f55019c139e45a8e94c2b1fdc
huanghz <huanghz@51kezhi.com>
2019-11-14 16:46:45 +0800

    初始化项目5

--- microservice/demo-service03/readme.md
+++ microservice/demo-service03/readme.md
@@ -1,3 +1,4 @@
 1. demo-service03
 2. demo-service04
-3. demo-service-api
+3. demo-service05
+4. demo-service-api
---
+++ microservice/demo-service05/src/main/java/hhz/demo/Service05Application.java
@@ -0,0 +1,19 @@
+package hhz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: Service05Application
+ * @Description: TODO(概况)
+ * @author: huanghz
+ * @date: 2019/11/14 0014 16:46
+ */
+@SpringBootApplication
+public class Service05Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Service05Application.class, args);
+    }
+}
