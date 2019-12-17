738aa62d4de943ea783d378868e43fda8b9dd04b
huanghz <827283849@qq.com>
2019-12-17 13:51:37 +0800

    搭建完成

---
+++ servicecomb-demo/oauth2-demo/src/main/java/hhz/entity/XcMenu.java
@@ -0,0 +1,14 @@
+package hhz.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName: XcMenu
+ * @Description: TODO(概况)
+ * @Author: huanghz
+ * @Date: 2019/12/17 13:40
+ */
+@Data
+public class XcMenu {
+    private String code;
+}
