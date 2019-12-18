c686dafd942e7c4f544cf88cec6f10fdc5456447
huanghz <827283849@qq.com>
2019-12-18 17:37:52 +0800

    初始化

---
+++ project-saas-hrm-vue/src/module-saas-clients/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card shadow="never">
+        saas页面
+      </el-card>
+     </div>
+  </div>
+</template>
+
+<script>
+import {list} from '@/api/base/saasClients'
+export default {
+  name: 'saas-clients-table-index',
+  data() {
+
+  },
+  methods: {
+    getList() {
+      list().then(result => {
+        console.log(result)
+      })
+    }
+  },
+  // 创建完毕状态
+  created() {
+    this.getList()
+  },
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.alert {
+  margin: 10px 0px 0px 0px;
+}
+.pagination {
+  margin-top: 10px;
+  text-align: right;
+}
+</style>
