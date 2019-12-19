19dde6a17abe432bfe18056377e7c9392725ce79
huanghz <827283849@qq.com>
2019-12-19 22:34:25 +0800

    1

--- project-saas-hrm-vue/src/api/base/saasClients.js
+++
@@ -1,4 +0,0 @@
-import {createAPI, createFormAPI} from '@/utils/request'
-
-export const list = data => createAPI('/company', 'get', data)
-export const detail = data => createAPI(`/company/${data.id}`, 'get', data)
--- project-saas-hrm-vue/src/api/example/table.js
+++
@@ -1,3 +0,0 @@
-import {createAPI, createFormAPI} from '@/utils/request'
-
-export const list = data => createAPI('https://mock.boxuegu.com/mock/29/table/list', 'get', data)
